f9d8bc46ffa22b84c33a6c9d41c09187df349390
GrimreaperVG <grimreapervg18@gmail.com>
2017-01-20 11:25:57 +0100

    fucties
    alle functies staan erin die erin horen

--- PHP/fucties.php
+++ PHP/fucties.php
@@ -13,16 +13,29 @@ function TweeGetallen($niveau){
 	elseif($niveau == 6)
 	{
 		$max = 100;
-	}	
+	}
+	else{}
+	
 	$min = 1;
 	$getal1 = mt_rand($min, $max);
 	$getal2 = mt_rand($min, $max);
 	$getallenPaar = array($getal1, $getal2);
-	return $getallenPaar;
+	return TweeGetallen;
 }
 //einde functie tweegetallen
 //begin functie voor feedback generator
 function Feedback(){
-	$fout_counter=0;
+	$inputAnswer=$_GET['answer'];
+	$name=$_GET['naam'];
+	if($inputAnswer == $info[3])
+	{
+		echo "Goedzo" . $name . "!";
+	}
+	else
+	{
+		echo "Jammer" . $name ."het juiste antwoord is" . $info[3];
+	}
+	return Feedback;
 }
 ?>
+setcookie('test', 'bullshit');
