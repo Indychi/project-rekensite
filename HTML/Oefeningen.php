254193e1303da8c4f3e6cb3b2e96f354cf515122
Indychi <carlochi15@gmail.com>
2017-01-25 15:21:37 +0100

    finaly finished the javascript part

--- HTML/Oefeningen.php
+++ HTML/Oefeningen.php
@@ -5,11 +5,8 @@
 		<meta charset="UTF-8">
 		<link rel="stylesheet" type="text/css" href="../CSS/CSS.css">
 		<link rel="icon" href="../image/plus.png">
-		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-		<link rel="stylesheet" href="/resources/demos/style.css">
-		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-		<?php
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<?php 
 			session_start();
 			if(isset($_GET['operator'])) {
 				$_SESSION['operator'] = $_GET['operator'];
@@ -22,57 +19,64 @@
 			}
 			$_SESSION['hoeveel'] = 1;
 			setcookie('feedback', '0');
-			
+		
+			if(isset($_POST['User_antwoord'])) {
+				$_SESSION['antwoord'][] = $_POST['User_antwoord'];
+				$vragen = count($_SESSION['antwoord']);
+				$progress_width = 900/20;
+				$progress = ($vragen)* $progress_width;
+				echo $vragen;
+				if($vragen == 20) {
+					$vragen = count($_SESSION['antwoord']);	
+				}
+				require('../PHP/feedback.php');
+				
+				if ($vragen == 20) 
+				{
+					header("location: Resultaten.php");
+				}
+			}
+			else {
+				$_SESSION['important'] = $_GET;
+			}
+			require('../PHP/Rekensommen.php');
+			$_SESSION['counter']=0;
+						
+			if($_SESSION['counter']  > 20) 
+			{
+				unset($_SESSION['counter']);
+			}
+			//session_destroy();
 		?>
+	<script>
+		$(document).ready(function(){
+			$(document).ready(function(){
+				$("#test").animate({width: '<?php echo $progress ?>px'});
+			});
+		});
+	</script>
 	</head>
 	<body>
-		<div id="som_area">
-			<div id="sommen">
-				<p>
-					<?php
-						if(isset($_POST['User_antwoord'])) {
-							$_SESSION['antwoord'][] = $_POST['User_antwoord'];
-							$vragen = count($_SESSION['antwoord']);
-							$progress = ($vragen + 1)*4.6;
-							echo $vragen;
-							if($vragen == 20) {
-							$vragen = count($_SESSION['antwoord']);	
-							require('../PHP/feedback.php');
-							
-							if ($vragen == 20) 
-							{
-								header("location: Resultaten.php");
-							}
-						}
-						else {
-							$_SESSION['important'] = $_GET;
-						}
-						require('../PHP/Rekensommen.php');
-						echo $info[0] . $info[2] . $info[1];
-						$_SESSION['counter']=0;
-						
-						if($_SESSION['counter']  > 20) 
-						{
-							unset($_SESSION['counter']);
-						}
-					?>
-				</p>
+		<div id="container">
+			<div id="som_area">
+				<div id="sommen">
+					<p>
+						<?php
+							echo $info[0] . $info[2] . $info[1];
+						?>
+					</p>
+				</div>
+				<div id="progress_container">
+					<div id="test" ></div>
+				</div>
+				<div id="Input_vak">
+					<form action="?" method="POST" id="antwoord" >
+						<input type="number" name="User_antwoord" placeholder="antwoord" required ></input>
+					</form>
+					<button type="submit" form="antwoord" value="Next" onkeypress="myMove()">Next</button>
+				</div>
 			</div>
-			<script>
-				$( function() {
-					$( "#Progress" ).progressbar({
-						value: <?php echo $progress; ?>
-					});
-				} );
-			</script>
-			<div id="Progress"></div>
-			<div id="Input_vak">
-				<form action="?" method="POST" id="antwoord" >
-					<input type="number" name="User_antwoord" placeholder="antwoord" onkeypress="move" required></input>
-				</form>
-				<button type="submit" form="antwoord" value="Next" onkeypress="move()">Next</button>
-			</div>
+			<div id="feedback_area"><?php print_r($_SESSION) ?></div>
 		</div>
-		<div id="feedback_area"><?php// print_r($_SESSION) ?></div>
 	</body>
 </html>
